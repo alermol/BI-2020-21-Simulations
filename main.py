eb27bea756923e40278eef9904557b95ef367603
alermol <ermol-2012@yandex.ru>
2020-12-11 20:26:53 +0300

    correct code style

--- main.py
+++ main.py
@@ -4,15 +4,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import itertools
 
-import sys
-import tqdm
-
 
 def mutate_hyb_site(site_seq, introns_divergency, introns_number):
+    '''
+    '''
     mutated_site_seq = list(site_seq)
-    split_positions = np.random.randint(low=0, high=len(site_seq),
-                                        size=introns_number * 2)
-    split_positions.sort()
+    split_positions = sorted(np.random.randint(low=0, high=len(site_seq),
+                                               size=introns_number * 2))
     split_positions = np.reshape(split_positions, (introns_number, 2))
 
     for i in split_positions:
@@ -21,8 +19,9 @@ def mutate_hyb_site(site_seq, introns_divergency, introns_number):
                                            size=changing_letters)
 
         for letter in pos_to_replace:
-            possible_replacement = [l for l in ('A', 'T', 'G', 'C')
-                                    if l != site_seq[letter]]
+            possible_replacement = list(
+                {'A', 'T', 'G', 'C'}.remove(site_seq[letter])
+            )
             mutated_site_seq[letter] = np.random.choice(possible_replacement,
                                                         size=1)[0]
     return ''.join(mutated_site_seq)
@@ -48,6 +47,7 @@ def main(generation_number,
          min_fragment_length,
          max_fragment_length,
          complemenarity_thrs):
+
     hybridization_site = np.random.choice(('A', 'T', 'G', 'C'),
                                           size=site_length,
                                           p=[(1 - gc_content) / 2,
@@ -68,7 +68,7 @@ def main(generation_number,
 
     result = {'R': 0, 'M': 0, 'B': 0, 'N': 0}
     for n in tqdm.tqdm(range(generation_number)):
-        successful_hybrisization = [None, None]
+        successful_hybridization = [None, None]
         for idx, t in enumerate(hybrid_sites):
             fragments = []
             while sum(list(map(len, fragments))) <= site_length:
@@ -109,18 +109,18 @@ def main(generation_number,
 
             if any([True for i in hybridizaton_res.values()
                     if i >= complemenarity_thrs]):
-                successful_hybrisization[idx] = True
+                successful_hybridization[idx] = True
             else:
-                successful_hybrisization[idx] = False
+                successful_hybridization[idx] = False
 
-        if (successful_hybrisization[0] == True and
-                successful_hybrisization[1] == False):
+        if (successful_hybridization[0] and
+                not successful_hybridization[1]):
             result['R'] += 1  # real target only
-        elif (successful_hybrisization[0] == False and
-              successful_hybrisization[1] == True):
+        elif (not successful_hybridization[0] and
+              successful_hybridization[1]):
             result['M'] += 1  # mutated target only
-        elif (successful_hybrisization[0] == False and
-              successful_hybrisization[1] == False):
+        elif (not successful_hybridization[0] and
+              not successful_hybridization[1]):
             result['N'] += 1  # no hybridization
         else:
             result['B'] += 1  # both targets
@@ -128,60 +128,13 @@ def main(generation_number,
 
 
 if __name__ == "__main__":
-    # np.random.seed(5671)
-
-    int_numbs = np.arange(0, 6, 1, dtype=int)
-    ry_array = np.zeros(int_numbs.shape)
-    my_array = np.zeros(int_numbs.shape)
-
-    internal_iterations = 50
-
-    for i in tqdm.tqdm(int_numbs):
-        ry_array_tmp = np.zeros(internal_iterations, dtype='uint8')
-        my_array_tmp = np.zeros(internal_iterations, dtype='uint8')
-        intr_numb = np.full(internal_iterations, i)
-        for ii in tqdm.tqdm(range(internal_iterations)):
-            data = main(generation_number=1000000,
-                        site_length=1100,
-                        gc_content=0.32,
-                        site_intron_number=i,
-                        intron_divergency=0.5,  # fraction of divergent bases in introns
-                        vector_length=3000,
-                        min_fragment_length=100,
-                        max_fragment_length=1100,
-                        complemenarity_thrs=0.2)
-            ry_array_tmp[ii] = data['R']
-            my_array_tmp[ii] = data['M']
-        ry_array[i] = np.mean(ry_array_tmp)
-        my_array[i] = np.mean(my_array_tmp)
-        plt.errorbar(i, np.mean(ry_array_tmp), ecolor='green',
-                     yerr=np.std(ry_array_tmp), elinewidth=1, capsize=3)
-        plt.errorbar(i, np.mean(my_array_tmp), ecolor='blue',
-                     yerr=np.std(my_array_tmp), elinewidth=1, capsize=3)
-
-    plt.plot(int_numbs, ry_array, c='green', marker='.', linestyle='--')
-    plt.plot(int_numbs, my_array, c='blue', marker='.', linestyle='--')
-
-    plt.grid()
-    plt.legend(['Успешные гибридизации с реальным таргетом',
-                'Успешные гибридизации с мутантным таргетом'],
-               loc='upper center', fontsize='xx-small',
-               bbox_to_anchor=(0.5, 1.06), shadow=True, ncol=2)
-    plt.title(('Зависимость количества событий гибридизации\n'
-               'от количества интронов в пробе'), pad=17.0)
-    plt.xticks(int_numbs)
-    plt.xlabel('Количество интронов')
-    plt.ylabel('Количество событий успешной гибридизации')
-    plt.savefig('plot5.png', dpi=600)
-    plt.close()
-
-    # print(
-# f'''
-# Experiment result:
-#
-# Hybridization with real target only: {data[0]}
-# Hybridization with mutated target only: {data[1]}
-# Hybridization with both targets: {data[3]}
-# No hybridization with both targets: {data[2]}
-# '''
-    # )
+    np.random.seed(5671)
+    data = main(generation_number=10,
+                site_length=1100,
+                gc_content=0.32,
+                site_intron_number=2,
+                intron_divergency=0.5,
+                vector_length=3000,
+                min_fragment_length=100,
+                max_fragment_length=1100,
+                complemenarity_thrs=0.8)
